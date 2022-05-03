c0c3b52fa59aab184a32861ea0641379322ff32d
benyamin parsa <benyaminparsa727@gmail.com>
2022-05-03 23:08:10 +0430

    fix

--- script.js
+++ script.js
@@ -4,3 +4,11 @@
 
 const scoreEl0 = document.getElementById('score--0');
 const scoreEl1 = document.getElementById('score--1');
+const DiceEl = document.querySelector('.dice');
+
+// Starting conditions //
+
+scoreEl0.textContent = 0;
+scoreEl1.textContent = 0;
+DiceEl.classList.add('hidden');
+
